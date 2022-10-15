3833a62cfda3df7cca832b9a5f0198a99f38c688
Flying Cat <94940211+FlyingCatDev@users.noreply.github.com>
2022-10-14 19:22:27 -0700

    Add files via upload

---
+++ Windows Activator/Windows Activator/Windows Activator.cpp
@@ -0,0 +1,118 @@
+// Windows Activator.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include<windows.h>
+#include <iostream>
+
+
+
+void Home()
+{
+    system("slmgr /ipk TX9XD-98N7V-6WMQ6-BX7FG-H8Q99");
+    system("slmgr /skms kms8.msguides.com");
+    system("slmgr /ato");
+}
+void HomeN()
+{
+    system("slmgr /ipk 3KHY7-WNT83-DGQKR-F7HPR-844BM");
+    system("slmgr /skms kms8.msguides.com");
+    system("slmgr /ato");
+
+
+
+}
+void HomeSingleLanguage()
+{
+    system("slmgr /ipk 7HNRX-D7KGG-3K4RQ-4WPJ4-YTDFH");
+    system("slmgr /skms kms8.msguides.com");
+    system("slmgr /ato");
+
+
+}
+void Professional()
+{
+    system("slmgr /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX");
+    system("slmgr /skms kms8.msguides.com");
+    system("slmgr /ato");
+
+
+}
+void ProfessionalN()
+{
+    system("slmgr /ipk MH37W-N47XK-V7XM9-C7227-GCQG9");
+    system("slmgr /skms kms8.msguides.com");
+    system("slmgr /ato");
+
+
+}
+void Education()
+{
+    system("slmgr /ipk NW6C2-QMPVW-D7KKK-3GKT6-VCFB2");
+    system("slmgr /skms kms8.msguides.com");
+    system("slmgr /ato");
+
+
+}
+void EducationN()
+{
+    system("slmgr /ipk 2WH4N-8QGBV-H22JP-CT43Q-MDWWJ");
+    system("slmgr /skms kms8.msguides.com");
+    system("slmgr /ato");
+
+
+}
+void Enterprise()
+{
+    system("slmgr /ipk NPPR9-FWDCX-D2C8J-H872K-2YT43");
+    system("slmgr /skms kms8.msguides.com");
+    system("slmgr /ato");
+
+
+}
+void EnterpriseN()
+{
+    system("slmgr /ipk DPH2V-TTNVB-4X9Q3-TJR4H-KHJW4");
+    system("slmgr /skms kms8.msguides.com");
+    system("slmgr /ato");
+
+
+}
+int main()
+{
+    SetConsoleTitleA("Seemo Activator, Cracked.io");
+    std::cout << "Hi, Lets activate your windows (Made by Seemo on Cracked.io.)\n";
+    std::cout << ("\n\n [1] Home\n [2] HomeN\n [3] HomeSingleLanguage\n [4] Professional \n [5] ProfessionalN \n [6] Education \n [7] EducationN \n [8] Enterprise \n [9] EnterpriseN\n\n ");
+    int Activateop;
+    std::cin >> Activateop;
+    switch (Activateop)
+    {
+    case 1:
+        Home();
+        break;
+    case 2: 
+        HomeN();
+        break;
+    case 3:
+        HomeSingleLanguage();
+        break;
+    case 4:
+        Professional();
+        break;
+    case 5:
+        ProfessionalN();
+        break;
+    case 6 :
+        Education();
+        break;
+    case 7:
+        EducationN();
+        break;
+
+    case 8 : 
+         Enterprise();
+            break;
+
+    case 9:
+        EnterpriseN();
+
+    }
+}
